db478a2cfbee4d637d42c9e5e14bb077adf19d94
AjeAyoub <ajeddabayoub.official@gmail.com>
2023-10-05 11:25:29 -0700

    program that prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-'''program that prints the result of the addition of all arguments'''
+
 if __name__ == "__main__":
     import sys
+
     numbers = sys.argv
     count_numbers = len(numbers) - 1
     result = 0
