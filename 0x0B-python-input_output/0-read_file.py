4156fa13be7777a5c30c3964d53073403b01565e
AjeAyoub <ajeddabayoub.official@gmail.com>
2023-11-07 07:11:24 -0800

    input & output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
+"""Defines a function for reading and displaying the contents of a text file."""
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    """
+    Reads the content of a UTF-8 encoded text file and prints it to the standard output.
+    
+    Args:
+        filename (str): The name of the file to be read. Defaults to an empty string.
+    """
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
