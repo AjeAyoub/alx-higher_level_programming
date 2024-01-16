be3b215fbab19586494e9e931fbb5d3957e291f6
AjeAyoub <ajeddabayoub.official@gmail.com>
2024-01-16 14:15:30 -0800

    First table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,8 +1,5 @@
 -- Script to create the first_table if it does not exist
 
-USE @dbName;
-
-/* Create the first_table if it does not exist */
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
