877c3155df3bd797a3ed7ef8cd07b12971455f6e
AjeAyoub <ajeddabayoub.official@gmail.com>
2024-01-18 14:34:59 -0800

    a script that lists all records of the table second_table ordered by name and score

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- a script that lists all records of the table second_table ordered by name and score
 
-SELECT name, score FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
