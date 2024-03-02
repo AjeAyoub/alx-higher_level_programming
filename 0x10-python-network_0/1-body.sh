f83798f413891875f7261d46d69d7678b35cc2e5
AjeAyoub <ajeddabayoub.official@gmail.com>
2024-03-02 13:15:20 -0800

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a GET request to the URL
-curl -s -o response.txt -w "%{http_code}" "$1" | {
-    read -r status_code
-    if [ "$status_code" == "200" ]; then
-        cat response.txt
-    fi
-    rm -f response.txt
-}
+curl -sL "$1"
